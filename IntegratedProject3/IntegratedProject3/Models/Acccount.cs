62979e7dcd48c5fec96a2bb36193afc412a33973
D-Vault <rossmcarthur97@yahoo.com>
2017-02-20 14:49:11 +0000

    Added Main POCO Classes

---
+++ IntegratedProject3/IntegratedProject3/Models/Acccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntegratedProject3.Models
+{
+    /// <summary>
+    /// Code First Representation of the Account table, with different criteria of their role in the system
+    /// </summary>
+    public class Acccount:ApplicationUser
+    {
+        public AccountType AccountType { get; set; }
+    }
+    /// <summary>
+    /// ENUM Representation of the system 
+    /// </summary>
+    public enum AccountType { Author, Admin, Distributee}
+
+    
+}
---
+++ IntegratedProject3/IntegratedProject3/Models/Version.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace IntegratedProject3.Models
+{
+    /// <summary>
+    /// Code first representation of the Version Table of the database
+    /// </summary>
+    public class Version
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Key]
+        public virtual Document document { get; set; }
+
+        [Key]
+        public double RevisionNum { get; set; }
+
+        public string DocumentTitle { get; set; }
+        
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:d}", ApplyFormatInEditMode =true)]
+        public DateTime DocCreationDate { get; set; }
+        
+        public virtual ICollection<Acccount>Distributees { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime ActivationDate { get; set; }
+
+    }
+}
