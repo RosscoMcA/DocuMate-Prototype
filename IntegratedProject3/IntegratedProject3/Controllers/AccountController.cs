bb2e3de28c8cca9844ec3c3e7998963a125cf9ea
D-Vault <rossmcarthur97@yahoo.com>
2017-03-01 22:22:58 +0000

    Fixed Login Issue for seeded User

--- IntegratedProject3/IntegratedProject3/Controllers/AccountController.cs
+++ IntegratedProject3/IntegratedProject3/Controllers/AccountController.cs
@@ -13,7 +13,7 @@ using IntegratedProject3.Models;
 namespace IntegratedProject3.Controllers
 {
     [Authorize]
-    public class AccountController : Controller
+    public class AccountController : RootController
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
@@ -72,10 +72,10 @@ namespace IntegratedProject3.Controllers
             {
                 return View(model);
             }
-
+            
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
